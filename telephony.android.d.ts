0690ed145f54ac5803183c013a9c3dd16423c0c4
patrickpereira <patrick@pereira.dk>
2018-08-30 00:59:56 +0400

    Fixes for more flexile permission handling

--- telephony.android.d.ts
+++ telephony.android.d.ts
@@ -1,1 +1,19 @@
+export interface Telephony {
+	countryCode?: string;
+	simOperator?: string;
+	carrierName?: string;
+	callState?: string;
+	dataActivity?: string;
+	networkType?: number;
+	phoneType?: number;
+	simState?: number;
+	isNetworkRoaming?: string;
+	mcc?: string;
+	mnc?: string;
+	phoneNumber?: string;
+	deviceId?: string;
+	deviceSoftwareVersion?: string;
+	simSerialNumber?: string;
+	subscriberId?: string;
+}
 export declare function Telephony(askPermission?: boolean): Promise<any>;
